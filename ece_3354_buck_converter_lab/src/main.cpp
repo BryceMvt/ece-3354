96d73804e9e1f6208a76222df3e74a538cd710ef
Bryce M. <3rycem@gmail.com>
2024-10-09 10:03:47 -0400

    feedback

--- ece_3354_buck_converter_lab/src/main.cpp
+++ ece_3354_buck_converter_lab/src/main.cpp
@@ -5,6 +5,11 @@
 
 #include <Arduino.h>
 
+const int SETPOINT = 512;       // Approximately 2.5V
+const int min_duty_cycle = 53;  // Approximately 20%
+const int max_duty_cycle = 212; // Approximately 80%
+
+
 void setup()
 {
   // GPIO #9 is our PWM output.
@@ -15,19 +20,32 @@ void setup()
   // Output-compare mode #2 "non-inverting" is assigned by setting bit COM1A1.
   // Non-prescaled clock source (prescaler = 1) is assigned by setting bit CS10.
   TCCR1A = (1 << WGM11);
-  TCCR1B = (1 << WGM13)  | (1 << WGM12) | (1 << CS10);
+  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10);
 
   // Timer1 TOP value must be set to 266 (265.6667) for 60kHz frequency.
   ICR1 = 265;
 
-  // Set the initial duty cycle. (133/266 = 50%)
+  // Set the initial duty cycle. (133/265 is about 50%)
   OCR1A = 133;
 
   // Enable the PWM output on GPIO #9.
   TCCR1A |= (1 << COM1A1);
+
+  delay(100);
 }
 
 void loop()
 {
+  // Measure the buck converter output voltage (1023 is about 5V)
+  int ADC_reading;
+  ADC_reading = analogRead(A0);
 
+  if (ADC_reading > SETPOINT)
+  {
+    OCR1A = constrain(OCR1A - 1, min_duty_cycle, max_duty_cycle);
+  }
+  else if (ADC_reading < SETPOINT)
+  {
+    OCR1A = constrain(OCR1A + 1, min_duty_cycle, max_duty_cycle);
+  }
 }
